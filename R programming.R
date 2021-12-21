085929ad3fb61215dd4f6278131a61fd73ddf3d7
Namrata Singh <88934779+nammoooo@users.noreply.github.com>
2021-12-21 17:31:04 +0530

    Add files via upload

---
+++ R programming.R
@@ -0,0 +1,64 @@
+> a = "mobile"
+> a
+[1] "mobile"
+> a="pen"
+> a
+[1] "pen"
+> a="makeup"
+> a
+[1] "makeup"
+> num1=3
+> num1
+[1] 3
+> class(num1)
+[1] "numeric"
+> char1="i love pizza"
+> char1
+[1] "i love pizza"
+> class("i love pizza")
+[1] "character"
+> log1=TRUE
+> log1
+[1] TRUE
+> class(log1)
+[1] "logical"
+> complex1=10-15i
+> complex1
+[1] 10-15i
+> class(complex1)
+[1] "complex"
+
+#Arithmetic Operators
+-----------------
+  Addition + 
+  Subtraction -
+  Multiplication *
+  Division /
+  
+ #Assignment Operators 
+-----------------------
+Equal  =
+Less than <-
+Greater than ->
+  
+#Relational Operators
+-----------------------
+  Greater than >
+  Less than <
+  Equal to ==
+  Not Equal to !=
+#Logical Operators
+  -------------
+  TRUE & (AND)
+  FALSE | (OR)
+  #Data Structures
+------------------------
+  #Vectors (single dimensional (no rows & colums) homogenous DS (similar type data)))
+=======================================================================================
+  vec1= c(1,2,3,4,5,6,7) (c is combine operators in vectors) numeric
+  vec2=c("a","b","c","d","e","f") character
+  vec3= c(T,F,T,F) logical (case sensitive only capital letters)
+    
+    
+    
+  
