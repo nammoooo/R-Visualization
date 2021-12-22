5c11a3f3106b9291001d319f6899e7a7dd68a221
Namrata Singh <88934779+nammoooo@users.noreply.github.com>
2021-12-22 17:07:07 +0530

    Add files via upload

---
+++ Varibales, Operators.R
@@ -0,0 +1,38 @@
+#Numeric value
+var1 = 20.50
+var1
+class(var1)
+#Charater value
+var2= "sam"
+var2
+class(var2)
+#Logical value
+var3= TRUE
+var3
+class(var3)
+#Complex Value
+var1= 20+30i
+var1
+class(var1)
+#Different types of Operators in R
+Assignments Operators - assign a value to particular value
+= a=8
+<- a<- "hello"
+->   200 -> a
+------------------------------
+Arithmetic Operators
+addition + 
+  > num1= 10
+> num2= 20
+> num1+num2
+subtraction (num1-num2)
+Multiplication (num1*num2)
+Division (num1/num2)
+--------------------------------------
+Relational Operators
+Greater than >, Less than < , Equal ==, Not equal !=
+----------------------------------------------------- 
+Logical Operators (case sensitive, always in capital letters)
+TRUE & AND, FALSE | OR
+& when both value are log1&log1 TRUE
+| when both operands are FALSE
